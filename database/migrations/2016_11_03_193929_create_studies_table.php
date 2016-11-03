290b5d85864df60dbc53405ba4d1fc84838aa4b1
David Martinez <davidmgilo@gmail.com>
2016-11-03 21:43:35 +0100

    added curriculumseeder

--- database/migrations/2016_11_03_193929_create_studies_table.php
+++ database/migrations/2016_11_03_193929_create_studies_table.php
@@ -17,7 +17,6 @@ class CreateStudiesTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('period_id');
-            $table->('');
             $table->timestamps();
         });
     }
