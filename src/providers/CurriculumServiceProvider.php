347deb8c40451999ad3b47f46e4e862e318a4859
David Martinez <davidmgilo@gmail.com>
2016-11-03 21:14:46 +0100

    serviceprovider migrations

--- src/providers/CurriculumServiceProvider.php
+++ src/providers/CurriculumServiceProvider.php
@@ -6,5 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class CurriculumServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        
+    }
 
+    public function boot()
+    {
+        //Inicialitzant el paquet cada cop que es carrega una pàgina
+
+        $this->loadMigrationsFrom(__DIR__.'../../database/migrations');
+    }
 }
